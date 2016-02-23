74c379899c00962a71cd53d031f474a4546d3bfd
JosemaUdeM <jose.megadeth@hotmail.com>
2016-02-23 11:30:22 -0500

    mapa logistico con 2 funciones

---
+++ euler.py
@@ -0,0 +1,23 @@
+#Codigo del dia 23 de febrero; dos funciones g() y f() para un nuevo mapa logistico.
+import numpy
+import matplotlib
+import matplotlib.pyplot
+
+def funciong(t,r):
+	return -r*t
+def funcionf(z,delta,r):
+	return z+delta*funciong(z,r)
+y0=0.1
+delta=0.01
+r=2.0
+y1=funcionf(y0,delta,r)
+print u
+
+n=100
+t=range(n)
+y=range(n)
+for i in range(n):
+	y1=funcionf(y0,delta,r)
+	y[i]=y1
+	
+	
