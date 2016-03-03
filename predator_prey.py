556a2ef9c1a0d09bc3c293a2781206bdb56a0ec0
JosemaUdeM <jose.megadeth@hotmail.com>
2016-03-03 17:36:07 -0500

    Update predator_prey.py

--- predator_prey.py
+++ predator_prey.py
@@ -1,3 +1,4 @@
+
 #marzo 1,2016
 #Tema:lokta-volterra model
 #objetivo: determinar la interaccion de 2 especies (predador y presa)
@@ -12,27 +13,31 @@ ky=2.0
 kyx=0.01
 kxy=0.01
 kx=1.06
-t=range(11)
-l=range(11)
-for i in range(11):
-	y1=y0+delta*ky*y0-kxy*x0*y0
-	x1=x0+delta*kyx*y0*x0-kx*x0
+y=range(101)
+x=range(101)
+t=range(101)
+y[0]=y0
+x[0]=x0
+for i in range(101):
+	y1=(delta*ky*y0)-(delta*kxy*x0*y0)+y0
 	y0=y1
+	x1=(delta*kyx*y0*x0)-(delta*kx*x0)+x0
 	x0=x1
-	t[i]=y1
-for r in range(11):
-	x1=x0+delta*kyx*y0*x0-kx*x0
-	y1=y0+delta*ky*y0-kxy*x0*y0
-	y0=y1
-	x0=x1
-	l[r]=x1
+	y[i]=y1
+	x[i]=x1
+	t[i]=i*delta
 
-print t
-print l
+print y
+print x
+ynew=numpy.array(y)
+xnew=numpy.array(x)
 tnew=numpy.array(t)
-lnew=numpy.array(l)
-print tnew
-print lnew
+print ynew
+print xnew
 
-matplotlib.pyplot.plot(tnew,lnew)
+matplotlib.pyplot.plot(xnew,ynew)
+matplotlib.pyplot.plot(ynew,xnew)
+matplotlib.pyplot.show()
+matplotlib.pyplot.plot(tnew,xnew)
+matplotlib.pyplot.plot(tnew,ynew)
 matplotlib.pyplot.show()
