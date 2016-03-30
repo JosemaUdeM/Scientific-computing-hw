c22078d2b6b345abeeb75cbe14cc847c88a16e88
JosemaUdeM <jose.megadeth@hotmail.com>
2016-03-30 17:56:55 -0500

    Hodking-Huxley

--- neuron.py
+++ neuron.py
@@ -8,7 +8,7 @@ x= np.linspace (0,10,num=10, endpoint=True)
 y= np.cos(-x**2 / 9.0)
 f= interp1d (x,y,kind = 'cubic')
 
-Delta=0.01
+Delta=0.001
 CM=0.5
 I=15
 V=-65
@@ -26,20 +26,36 @@ n=0.317
 m=0.05
 h=0.6
 T=6.3
-phi=3**(T-6.3)/10
+phi=3**((T-6.3)/10)
 
 
-def alfah(phi,V):
-	ah=phi(0.07 np.exp(V/20))
-	return ah
+
+ah=phi*(0.07 np.exp(V/20))
 	
-def Betan(phi,V)
-	Bn=phi(0.125 np.exp(V/80))
-	return Bn
-
-def Betam(phi,V)
-	Bm=phi(4 np.exp(V/18))
-	return Bm
-
-
 	
+
+Bn=phi*(0.125 np.exp(V/80))
+	
+
+Bm=phi*(4 np.exp(V/18))
+	
+
+
+an=phi*(0.01*(V+10)/(np.exp((V+10)/10)-1))
+	
+
+am=phi*(0.01*(V+25)/(np.exp((V+25)/10)-1))
+	
+
+
+Bh=phi*(1/(np.exp((V+30)/10)+1))
+	
+for i in range(0,6)
+	n1= n+delta*(an*(1-n)-Bn*n)
+	n=n1
+for t in range(0,6)
+	m1= m+delta*(am*(1-m)-Bm*m)
+	m=m1
+for l in range(0,6)
+	h1= h+delta*(ah*(1-h)-Bh*h)
+	h=h1
