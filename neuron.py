9229420efac93a70872c5db1dd70043c26cc721a
JosemaUdeM <jose.megadeth@hotmail.com>
2016-04-04 21:55:33 -0500

    Update neuron.py

--- neuron.py
+++ neuron.py
@@ -5,58 +5,34 @@ import sys
 import math 
 from scipy.interpolate import interp1d
 
-delta=0.003
+x= np.linspace (0,5,num=10, endpoint=True)
+y= np.cos(-x**2 / 9.0)
+f= interp1d(x,y,kind = 'cubic')
+
 Delta=0.001
-
-valnum=5/Delta
-x= np.linspace (0,5,num=valnum, endpoint=True)
-#y= np.cos(-x**2 / 9.0)
-#f= interp1d (x,y,kind = 'cubic')
-
-
-
-
-
-CM=0.5
+delta=0.003
+CM=1
 I=15
-Vk=-77
+
+Vk=-12
 ki=150
 ko=5.5
-VNa=50
+VNa=115
 Nai=15
 Nao=150
-VL=-54.4
+VL=10.599
 gK=36
 gNa=120
 gL=0.3
 
 T=6.3
 
-Q=np.zeros(len(x))
-Q1=np.zeros(len(x))
-Q2=np.zeros(len(x))
-Q3=np.zeros(len(x))
 
-V=-65
-n=0.317
-m=0.05
-h=0.6
 
 def phi(T): 
 	return 3**((T-6.3)/10)
 
 
-
-def IK(n, V):
-	iK = gK*(n**4)*(V-VK)
-	return iK
-
-def IL(V):
-	iL = gL*(V-VL)
-	return iL
-def INa(m, h, V):
-	iNa = gNa*(m**3)*h*(V-VNa)
-	return iNa
 
 def an(V):
 	return phi(T)*(0.01*(10-V)/(math.exp((10-V)/10)-1))
@@ -69,49 +45,67 @@ def am(V):
 	return phi(T)*(0.01*(25-V)/(math.exp((25-V)/10)-1))
 	
 def Bm(V):
-	return phi(T)*(4 *math.exp(-V/18))
+	return phi(T)*4*math.exp(-V/18)
 
 	
 def ah(V):
-	return phi(T)*(0.07 *math.exp(-V/20))
+	return phi(T)*0.07*math.exp(-V/20)
 
 def Bh(V):
-	return phi(T)*(1/(math.exp((30-V)/10)+1))
+	return phi(T)*1/(math.exp((30-V)/10)+1)
+
+def IK(V,n): 
+	return gK*n**4*(V-Vk)
+
+def IL(V): 
+	return gL*(V-VL)
+def INa(V, m, h): 
+	return gNa*m**3*h*(V-VNa)
 
 
+def fp(V, n, m, h): 
+	Vdt = (I-IK(V, n)-INa(V, m, h) -IL(V))/CM
+	ndt = an(V)*(1-n) -Bn(V)*n
+	mdt = am(V)*(1-m) - Bm(V)*m
+	hdt = ah(V)*(1-h) -Bh(V)*h
+	return Vdt, ndt, mdt, hdt 
 
 
-def Funciones(n,m,h):
-#	V1 = (I-IK(n, V)-INa(m, h, V)-IL(V))/CM
-	n1 = an(V)*(1-n)-Bn(V)*n 
-	m1 = am(V)*(1-m)-Bm(V)*m
-	h1 = ah(V)*(1-h)-Bh(V)*h  
-	return n1, m1, h1
-	
-for i in range (len(x)):
+t = np.linspace(0, 5, num =20000, endpoint=True)
+n = np.zeros(len(t))
+m = np.zeros(len(t))
+h = np.zeros(len(t))
+V = np.zeros(len(t))
 
-	N,M,H = Funciones (n,m,h)
-	N1=n+delta*N
-	M1=m+delta*M
-	H1=h+delta*H
-#	n1= n+delta*(an*(1-n)-Bn*n)
-#	m1= m+delta*(am*(1-m)-Bm*m)
-#	h1= h+delta*(ah*(1-h)-Bh*h)
-	
-	Q[i]=N1
-	Q1[i]=M1
-	Q2[i]=H1
+V[0]=-65
+n[0]=0.317
+m[0]=0.05
+h[0]=0.6
 
+for i in range(len(t)-1):
 
-	
-	n=N1
-	m=M1
-	h=H1
+   Vdt, ndt, mdt, hdt = fp(V[i], n[i], m[i], h[i])
 
+   Vdtt, ndtt, mdtt, hdtt = fp(V[i] + (0.5*delta*Vdt), n[i] + (0.5*delta*ndt), m[i] + (0.5*delta*mdt), h[i] + (0.5*delta*hdt))
+   V[i+1] = V[i] + (delta*Vdtt)	
+   n[i+1] = n[i] + (delta*ndtt)
+   m[i+1] = m[i] + (delta*mdtt)
+   h[i+1] = h[i] + (delta*hdtt)
 
-	
-plt.plot(x,Q)
-plt.plot(x,Q1)
-plt.plot(x,Q2)
+										
 
+## Graphics
+plt.plot(t, m, 'r', label='m')
+plt.plot(t, h, 'g', label='h')
+plt.plot(t, n, 'b', label='n')
+plt.legend()
+plt.show()
+
+plt.plot(V,n)
+plt.title('Phase space Voltage vs n')
+plt.show()
+
+plt.plot(t,V)
+plt.xlabel('t')
+plt.ylabel('V(t)')
 plt.show()
